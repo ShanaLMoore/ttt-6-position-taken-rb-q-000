15de9f85e6cbe3aaf7f50b7ce6838033ab8de503
Shana L Moore <shana.lavina.moore@gmail.com>
2015-10-09 22:14:29 -0700

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken? (board, position)
   if (board[position] == " ") || (board[position] == "") || (board[position] == nil)
     return false
-  elsif (board[position] == "X") || (board[position] == "O")
+  else (board[position] == "X") || (board[position] == "O")
     return true
   end
 end
